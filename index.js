8a5a10742edeb4a82948cf592894f9b08728a257
farhadbgd <farhadbgd@outlook.com>
2022-11-23 23:34:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Wow server is running');
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
