375fb42a2b3df181f5c604fac162131432a279de
farhadbgd <farhadbgd@outlook.com>
2022-11-24 00:11:44 +0600

    vercel file create

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
+// const port = process.env.REACT_APP_API_URL;
 const port = process.env.PORT || 5000;
 
 // middleware
